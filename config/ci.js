a0f61c4f41133dbbca6c6e53965b39f19dbe3895
Muhammad Raza <faisalraza32@hotmail.com>
2020-04-05 16:31:24 -0400

    add travis.yml file for continuour integration (ci)

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: '641151808178-ticug4l51o4cdn5vvskdfjja9d32ruv5.apps.googleusercontent.com',
+  googleClientSecret: 'hvw45iJBHnaG7SVED6fdzkA6',
+  mongoURI1: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: 'somelongstring',
+  redisUrl: 'redis://127.0.0.1:6379',
+};
